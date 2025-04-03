c283a29ac4a5404aa98f4f99b8fec26c91ef1e5b
João Capucho <capucho@jcapucho.com>
2025-04-03 18:37:53 +0100

    gateway: Add validate endpoint

--- gateway/app/endpoints/smsotp/__init__.py
+++ gateway/app/endpoints/smsotp/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from . import send
+from . import send, validate
 
 router = APIRouter(prefix="/one-time-password-sms/v1")
 router.include_router(send.router)
+router.include_router(validate.router)
