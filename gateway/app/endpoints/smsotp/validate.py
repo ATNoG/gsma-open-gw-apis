0f4a12aa083c12d52dbe097173ea1ffaefc2a47b
João Capucho <capucho@jcapucho.com>
2025-04-04 10:14:52 +0100

    gateway: Return proper error responses and document with openapi

--- gateway/app/endpoints/smsotp/validate.py
+++ gateway/app/endpoints/smsotp/validate.py
@@ -1,26 +1,29 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, status
 
-from app.interfaces.otp import (
-    OTPExpiredCodeError,
-    OTPInvalidCodeError,
-    OTPNotFoundError,
-    OTPTooManyAttemptsError,
-)
+from typing import Any, Union
+
 from app.drivers.otp import OTPInterfaceDep
+from app.schemas import ErrorInfo
 from app.schemas.smsotp import ValidateCodeBody
+
 
 router = APIRouter()
 
+responses: dict[Union[int, str], dict[str, Any]] = {
+    400: {
+        "description": """\
+Problem with the client request. In addition to regular scenario of `INVALID_ARGUMENT`, another scenarios may exist:
+  - Too many unsuccessful attempts (`{"code": "ONE_TIME_PASSWORD_SMS.VERIFICATION_FAILED","message": "The maximum number of attempts for this authenticationId was exceeded without providing a valid OTP"}`)
+  - Expired authenticationId (`{"code": "ONE_TIME_PASSWORD_SMS.VERIFICATION_EXPIRED","message": "The authenticationId is no longer valid"}`)
+  - OTP is not valid for the provided authenticationId (`{"code": "ONE_TIME_PASSWORD_SMS.INVALID_OTP","message": "The provided OTP is not valid for this authenticationId"}`)
+        """,
+        "model": ErrorInfo,
+    }
+}
 
-@router.post("/validate-code", status_code=status.HTTP_204_NO_CONTENT)
+
+@router.post(
+    "/validate-code", status_code=status.HTTP_204_NO_CONTENT, responses=responses
+)
 async def send_code(body: ValidateCodeBody, otp_interface: OTPInterfaceDep) -> None:
-    try:
-        await otp_interface.verify_otp(body.authenticationId, body.code)
-    except (OTPNotFoundError, OTPInvalidCodeError):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not valid")
-    except OTPTooManyAttemptsError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Too many attempts"
-        )
-    except OTPExpiredCodeError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Expired")
+    await otp_interface.verify_otp(body.authenticationId, body.code)
