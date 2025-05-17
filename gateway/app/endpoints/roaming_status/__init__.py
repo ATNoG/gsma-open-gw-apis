0490cce1cbd2b6187190ee3d650c9804b537de58
João Capucho <capucho@jcapucho.com>
2025-05-17 20:28:46 +0100

    Add roaming status endpoint

---
+++ gateway/app/endpoints/roaming_status/__init__.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+from app.exceptions import MissingDevice
+from app.schemas.roaming_status import RoamingStatusRequest, RoamingStatusResponse
+from app.drivers.roaming_status import RoamingStatusInterfaceDep
+from app.utils.mcc_to_country_code import get_country_names
+
+router = APIRouter(prefix="/device-roaming-status/v1")
+
+
+@router.post(
+    "/retrieve", response_model_exclude_unset=True, response_model_exclude_none=True
+)
+async def retrieve_roaming_status(
+    body: RoamingStatusRequest,
+    roaming_status_interface: RoamingStatusInterfaceDep,
+) -> RoamingStatusResponse:
+    if body.device is None:
+        raise MissingDevice()
+
+    res = await roaming_status_interface.get_roaming_status(body.device)
+
+    if res.countryCode is not None and res.countryName is None:
+        res.countryName = get_country_names(res.countryCode)
+
+    return res
