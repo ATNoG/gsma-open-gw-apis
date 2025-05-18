b509f6300ff0f6954f6e8de3e6a4729fcb0c92d4
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-05-18 23:33:35 +0100

    feat(final-demo): Improved landing page

--- demos/final-demo/src/routes/index.tsx
+++ demos/final-demo/src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,16 +8,44 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div>
-      Final demo
-      <ul className="ml-2 list-inside list-disc">
-        <li>
-          <Link to="/infotainment">Infotainment</Link>
+    <main className="container mx-auto my-20 px-4">
+      <h1 className="mb-2 text-center text-6xl font-extrabold">Final Demo</h1>
+      <p className="text-foreground/65 mb-12 text-center text-lg font-semibold">
+        Demo websites for the final presentation
+      </p>
+      <p className="mb-4 text-3xl font-bold">Available pages:</p>
+      <ul className="grid grid-cols-[auto_auto_1fr] divide-y-1 text-lg">
+        <li className="text-foreground/70 col-span-3 grid grid-cols-subgrid items-center pb-2">
+          <Link
+            className={cn(
+              buttonVariants({ size: "lg" }),
+              "mr-4 text-xl font-semibold",
+            )}
+            to="/infotainment"
+          >
+            Infotainment
+          </Link>
+          -
+          <span className="ml-4">
+            Website for the video streaming service inside the truck
+          </span>
         </li>
-        <li>
-          <Link to="/management">Fleet Management</Link>
+        <li className="text-foreground/70 col-span-3 grid grid-cols-subgrid items-center pt-2">
+          <Link
+            className={cn(
+              buttonVariants({ size: "lg" }),
+              "mr-4 text-xl font-semibold",
+            )}
+            to="/management"
+          >
+            Fleet Management
+          </Link>
+          -
+          <span className="ml-4">
+            Website for managemant of the truck fleet
+          </span>
         </li>
       </ul>
-    </div>
+    </main>
   );
 }
