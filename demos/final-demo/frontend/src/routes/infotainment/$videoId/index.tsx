924203b8c600f93acb2564a583d2dfc2f04fc51f
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-05-20 10:50:03 +0100

    feat(demo-infotainment): video list page and video page

--- demos/final-demo/frontend/src/routes/infotainment/$videoId/index.tsx
+++ demos/final-demo/frontend/src/routes/infotainment/$videoId/index.tsx
@@ -1,9 +1,59 @@
+import { videosById } from "@/data/videos";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 
 export const Route = createFileRoute("/infotainment/$videoId/")({
   component: RouteComponent,
 });
 
+// const YT_EMBED_BASE_URL = "https://www.youtube.com/embed";
+//
+const aspect = 16 / 9;
+
+function calcDimensions(width: number, height: number) {
+  const scaledHeight = height * aspect;
+  if (width < scaledHeight) {
+    return {
+      width,
+      height: width / aspect,
+    };
+  } else if (width > scaledHeight) {
+    return {
+      height,
+      width: scaledHeight,
+    };
+  } else {
+    return { width, height };
+  }
+}
+
 function RouteComponent() {
-  return <div>Video</div>;
+  const { videoId } = Route.useParams();
+  const video = videosById.get(videoId);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  useEffect(() => {
+    const onResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+  if (video === undefined) return "Video not found";
+
+  const { width: vidWidth, height: vidHeight } = calcDimensions(width, height);
+  return (
+    <main className="grid h-screen w-full place-items-center bg-black">
+      <YouTube
+        style={{ width: `${vidWidth}px`, height: `${vidHeight}px` }}
+        iframeClassName="w-full h-full focus:outline-none"
+        onPlaybackQualityChange={(event) => {
+          console.log(event.target.setPlaybackQuality("hd1080"));
+        }}
+        videoId={videoId}
+      />
+    </main>
+  );
 }
--- demos/final-demo/frontend/src/routes/infotainment/index.tsx
+++ demos/final-demo/frontend/src/routes/infotainment/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { videoData } from "@/data/videos";
+import { createFileRoute, Link, redirect } from "@tanstack/react-router";
 import { userStore } from "../infotainment";
 
 export const Route = createFileRoute("/infotainment/")({
@@ -10,7 +11,50 @@ export const Route = createFileRoute("/infotainment/")({
     }
   },
 });
+function formatTime(totalSeconds: number): string {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
+  const paddedMinutes = minutes.toString().padStart(2, "0");
+  const paddedSeconds = seconds.toString().padStart(2, "0");
+
+  if (hours > 0) {
+    const paddedHours = hours.toString().padStart(2, "0");
+    return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+  } else {
+    return `${paddedMinutes}:${paddedSeconds}`;
+  }
+}
 function RouteComponent() {
-  return <div>List of videos</div>;
+  return (
+    <main className="container mx-auto my-20 px-4">
+      <h1 className="mb-12 text-center text-3xl font-bold">Videos</h1>
+      <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {videoData.map((video) => (
+          <li
+            key={video.id}
+            className="border-foreground/20 hover:bg-foreground/10 group overflow-hidden rounded-md border shadow"
+          >
+            <Link to="/infotainment/$videoId" params={{ videoId: video.id }}>
+              <div className="relative">
+                <img
+                  className="block aspect-video w-full object-cover object-center"
+                  src={`https://img.youtube.com/vi/${video.id}/0.jpg`}
+                />
+                <div className="text-primary-foreground text-semibold absolute right-2 bottom-2 rounded-sm bg-black px-2 py-1 text-sm">
+                  {formatTime(video.duration)}
+                </div>
+              </div>
+              <section className="px-4 py-2">
+                <p className="text-sm font-semibold group-hover:underline">
+                  {video.title}
+                </p>
+              </section>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
