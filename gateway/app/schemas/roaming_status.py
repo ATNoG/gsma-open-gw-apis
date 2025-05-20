b132242324729f0375fc058837338ff0e597d8b9
João Capucho <capucho@jcapucho.com>
2025-05-20 16:03:11 +0100

    Add roaming status subscriptions schemas

--- gateway/app/schemas/roaming_status.py
+++ gateway/app/schemas/roaming_status.py
@@ -1,9 +1,12 @@
-from typing import Optional, Annotated, List
+from enum import Enum
+from typing import Optional, Annotated, List, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, TypeAdapter
 
 from app.schemas.device import Device
 from app.schemas.common import LastStatusTime
+import app.schemas.subscriptions as subscriptions
+from app.schemas.subscriptions import SubscriptionId, TerminationReason
 
 ActiveRoaming = Annotated[
     bool, Field(description="Roaming status. True, if it is roaming")
@@ -32,3 +35,70 @@ class RoamingStatusResponse(BaseModel):
 
 class RoamingStatusRequest(BaseModel):
     device: Optional[Device] = None
+
+
+class NotificationEventType(str, Enum):
+    v0_roaming_status = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-status"
+    )
+    v0_roaming_on = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-on"
+    )
+    v0_roaming_off = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-off"
+    )
+    v0_roaming_change_country = "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-change-country"
+    v0_subscription_ends = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.subscription-ends"
+    )
+
+
+class SubscriptionEventType(str, Enum):
+    v0_roaming_status = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-status"
+    )
+    v0_roaming_on = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-on"
+    )
+    v0_roaming_off = (
+        "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-off"
+    )
+    v0_roaming_change_country = "org.camaraproject.device-roaming-status-subscriptions.v0.roaming-change-country"
+
+
+class BasicDeviceEventData(BaseModel):
+    device: Optional[Device] = None
+    subscriptionId: SubscriptionId
+
+
+class RoamingStatus(BasicDeviceEventData):
+    roaming: bool = Field(..., description="Roaming status. True, if it is roaming.")
+    countryCode: Optional[CountryCode] = None
+    countryName: Optional[CountryName] = None
+
+
+class RoamingChangeCountry(BasicDeviceEventData):
+    countryCode: CountryCode
+    countryName: CountryName
+
+
+class SubscriptionEnds(BasicDeviceEventData):
+    terminationReason: TerminationReason
+    terminationDescription: Optional[str] = None
+
+
+class CreateSubscriptionDetail(BaseModel):
+    device: Optional[Device] = None
+
+
+CloudEventData = Union[
+    BasicDeviceEventData, RoamingStatus, RoamingChangeCountry, SubscriptionEnds
+]
+CloudEvent = subscriptions.CloudEvent[NotificationEventType, CloudEventData]
+Subscription = subscriptions.Subscription[
+    SubscriptionEventType, CreateSubscriptionDetail
+]
+SubscriptionTypeAdapter: TypeAdapter[Subscription] = TypeAdapter(Subscription)
+SubscriptionRequest = subscriptions.SubscriptionRequest[
+    SubscriptionEventType, CreateSubscriptionDetail
+]
