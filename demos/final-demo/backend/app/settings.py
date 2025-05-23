d58b2dde60ca4d05eb296411580e28d279147b6c
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-05-23 16:44:01 +0100

    feat(final-demo): Add geofencing queue to settings

--- demos/final-demo/backend/app/settings.py
+++ demos/final-demo/backend/app/settings.py
@@ -1,4 +1,6 @@
-from pydantic import AnyHttpUrl
+from typing import Annotated
+
+from pydantic import AnyHttpUrl, Field
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
@@ -7,11 +9,18 @@ from pydantic_settings import (
 )
 
 
+class QueueGeofencingSettings(BaseSettings):
+    latitude: Annotated[float, Field(ge=-90, le=90)] = 38.000392
+    longitude: Annotated[float, Field(ge=-180, le=180)] = 23.818231
+    radius: Annotated[int, Field(gt=0)] = 65
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         toml_file="config.toml",
     )
 
+    queue_geofencing: QueueGeofencingSettings
     gateway_url: AnyHttpUrl = AnyHttpUrl("http://localhost:8000")
 
     @classmethod
