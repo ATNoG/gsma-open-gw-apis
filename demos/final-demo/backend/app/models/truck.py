95ec34695040daa8dafa4a3857534411009a62c0
iavcoelho <iavcoelho@ua.pt>
2025-05-23 17:24:06 +0100

    demo: added qos to backend

--- demos/final-demo/backend/app/models/truck.py
+++ demos/final-demo/backend/app/models/truck.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 
 
 class Truck(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     phoneNumber: str
     isQueued: bool = Field(default=False)
     isReachable: bool = Field(default=False)
---
+++ demos/final-demo/backend/app/services/qos.py
@@ -0,0 +1,27 @@
+import httpx
+
+from app.models.truck import Truck
+from app.settings import settings
+
+
+class QosService:
+
+    def __init__(self) -> None:
+        self.httpx_client = httpx.AsyncClient(base_url=str(settings.gateway_url))
+
+    async def increase_bandwidth(self, truck: Truck) -> str:
+        data = {
+            "device": {
+                "phoneNumber": truck.phoneNumber,
+            },
+            "qosProfile": "fast",
+        }
+
+        doc = await self.httpx_client.post(
+            url="/qod-provisioning/v0.2/device-qos", json=data
+        )
+
+        if not doc.is_success:
+            raise RuntimeError("Bandwidth could not be increased: ", doc.json())
+
+        return doc.json().get("provisioningId")
